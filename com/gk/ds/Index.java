ae71f1e366ddc0097eba0709572d00a85fc1321e
nnagagee <nakka.naga.geetha.krishna@oracle.com>
2026-08-16 13:08:26 +0530

    started DP

---
+++ com/gk/ds/Index.java
@@ -0,0 +1,8 @@
+package com.gk.ds;
+
+public class Index {
+//will include the list of all the data structures and problem patterns here ,this is an index page for me to revisit the class I want to understand
+    public static void main(String[] args) {
+        System.out.println("Dynamic Programming");
+    }
+}
---
+++ com/gk/ds/dynamicProgramming/knapsac/knapsac1.java
@@ -0,0 +1,5 @@
+package com.gk.ds.dynamicProgramming.knapsac;
+
+public class knapsac1 {
+
+}
