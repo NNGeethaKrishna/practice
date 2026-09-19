aadaaa0993a7d5205a1da5890922e2bb9799044c
nnagagee <nakka.naga.geetha.krishna@oracle.com>
2026-09-19 15:52:42 +0530

    insertion sort

--- com/gk/ds/Sorting/SortingTechiniques.java
+++ com/gk/ds/Sorting/SortingTechiniques.java
@@ -1,5 +1,7 @@
 package com.gk.ds.Sorting;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class SortingTechiniques {
@@ -9,6 +11,11 @@ public class SortingTechiniques {
     //at the end u will have the requred sorted array
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+        System.out.println("which sorting technique do u want o use:");
+        System.out.println("Choose 1 for Selection sort");
+        System.out.println("Choose 2 for bubbleSort");
+        System.out.println("Choose 3 for insertionSort");
+        int choice  = sc.nextInt();
         System.out.println("Enter the number of digits to be sorted:");
         int n = sc.nextInt();
         int[] arr = new int[n];
@@ -16,8 +23,16 @@ public class SortingTechiniques {
         for(int i =0;i<n;i++){
             arr[i] = sc.nextInt();
         }
-        selectionSort(arr);
-        System.out.println("Sorted list is : ");
+        switch(choice){
+            case 1:selectionSort(arr);
+            System.out.println("Sorted list using Selection sort is: ");
+            case 2:bubbleSort(arr);
+            System.out.println("Sorted list using Bubble sort is: ");
+            case 3:insertionSort(arr);
+            System.out.println("Sorted list using Bubble sort is: ");
+            default:Arrays.sort(arr);
+        }        
+        
         for(int i =0;i<n;i++){
             System.out.print(String.format("%d ", arr[i]));
         }
@@ -41,6 +56,36 @@ public class SortingTechiniques {
             arr[jmin]=tmp; 
         }
     }
-    
+    //worst case time complexity is O(n*n)
+    public static void bubbleSort(int[] arr){
+        int n = arr.length;
+        for(int i = n-1;i>=0;i--){
+            for(int j =0 ; j<i;j++){
+                if(arr[j]>arr[j+1]){
+                    int tmp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1]= tmp;
+                }
+            }
+        }
+    }
+    //time conplexity is O(n*n)
+    public static void insertionSort(int[] nums) {
+        int n = nums.length; // Size of the array 
+        
+        // For every element in the array 
+        for (int i = 1; i < n; i++) {
+            int key = nums[i]; // Current element as key 
+            int j = i - 1;
+            
+            // Shift elements that are greater than key by one position
+            while (j >= 0 && nums[j] > key) {
+                nums[j + 1] = nums[j];
+                j--;
+            }
+            
+            nums[j + 1] = key; // Insert key at correct position
+        }
+    }
 
 }
